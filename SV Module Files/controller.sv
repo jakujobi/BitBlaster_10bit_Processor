071664bacf492315811ce27e924f502078a3613e
jakujobi <akujobi365@gmail.com>
2023-11-26 13:05:58 -0600

    committing the project so far

--- SV Module Files/controller.sv
+++ SV Module Files/controller.sv
@@ -62,22 +62,22 @@ if (T == 2'b00) begin
     // case (T)
     // 2'b00: //!____________________________________________________________________
         // //Set everything else to 0
-        //ENW = 0;
-        //Gout = 0;
+        ENW = 0;
+        Gout = 0;
 
-        // ENR = 0;
-        // Ain = 0;
-        // Gin = 0;
-        // Clr = 0;  
+        ENR = 0;
+        Ain = 0;
+        Gin = 0;
+        Clr = 0;  
         IMM = 10'bz;                //Don't let IMM write to the bus from the controller
 
-        // Ext = 1;                    //Allow external data input
-        // IRin = 1;                   //Let the instruction register read
+        Ext = 1;                    //Allow external data input
+        IRin = 1;                   //Let the instruction register read
 
 end else if (T == 2'b01) begin
     // 2'b01: //!_____________________________________________________________________
-        // Ext = 0;                    //Don't allow external data input to the bus
-        // IRin = 0;                   //Stop the instruction register from reading
+        Ext = 0;                    //Don't allow external data input to the bus
+        IRin = 0;                   //Stop the instruction register from reading
 
         //Rx = INST[7:6];             //Get the Rx register
         
@@ -85,68 +85,68 @@ end else if (T == 2'b01) begin
             IMM[2:0] = INST[6:4];   //Get the immediate value from the instruction
             IMM[9:3] = 7'b0000000;  //Set the sign bit to 0
 
-            // Rin = INST[7:6];               //Load the data into the Rx register
-            // ENR = 1;              //Let the register file read
-            // Clr = 1;                //Done with the operation, reset the counter
+            Rin = INST[7:6];               //Load the data into the Rx register
+            ENR = 1;              //Let the register file read
+            Clr = 1;                //Done with the operation, reset the counter
         end
         
         else if (INST[9:8] == 2'b00 && INST[3:0] == COPY) begin //4'b0001 equals to COPY
-            // Rout = INST[7:6];              //Prep the Rx register to write
-            // ENW = 1;                //Let the register file write to the bus
+            Rout = INST[7:6];              //Prep the Rx register to write
+            ENW = 1;                //Let the register file write to the bus
 
-            // Rin = INST[5:4];               //Load the data into the Rx register
-            // ENR = 1;              //Let the register file read
-            // Clr = 1;                //Done with the operation, reset the counter
+            Rin = INST[5:4];               //Load the data into the Rx register
+            ENR = 1;              //Let the register file read
+            Clr = 1;                //Done with the operation, reset the counter
         end
 
         else begin
             // Rout = Rx;              //Prep the Rx register to write
-            // ENW = 1;                //Let the register file write to the bus
+            ENW = 1;                //Let the register file write to the bus
 
-            // Ain = 1;                //Let the A register save the value from the bus
+            Ain = 1;                //Let the A register save the value from the bus
         end
 end
 else if (T == 2'b10) begin
     //2'b10: //!_____________________________________________________________________
-        // Ain = 0;                //Stop the A register from saving the value from the bus
-        // Gin = 1;                //Let the G register save the value from the bus
+        Ain = 0;                //Stop the A register from saving the value from the bus
+        Gin = 1;                //Let the G register save the value from the bus
         if (INST[9:8] == 2'b00) begin
 
              IMM = 10'bz;            //Don't let IMM write to the bus from the controller
 
         //     Ry = INST [5:4];             //Get the Ry register from the Rx register
         //     Rout = Ry;              //Prep the Ry register to write
-        //     ENW = 1;                //Let the register file write to the bus
+            ENW = 1;                //Let the register file write to the bus
 
-        //     ALUcont = INST[3:0];    //Get the ALU operation from the instruction
+            ALUcont = INST[3:0];    //Get the ALU operation from the instruction
 
         end else if (INST[9:8] == 2'b10) begin
-            // ENW = 0;                //Don't let the register file write to the bus
+            ENW = 0;                //Don't let the register file write to the bus
             
             IMM[5:0] = INST[5:0];        //Get the immediate value from the instruction
             IMM[9:6] = 4'b0000;       //Set the other bit to 0
-            // ALUcont = 4'b0010;            //Get the ALU operation from the instruction
+            ALUcont = 4'b0010;            //Get the ALU operation from the instruction
 
         end else if (INST[9:8] == 11) begin
-        //     ENW = 0;                //Don't let the register file write to the bus
-           IMM[5:0] = INST[5:0];        //Get the immediate value from the instruction
-           IMM[10:6] = 4'b0000;       //Set the other bit to 0
-        //     ALUcont = 4'b0011;            //Get the ALU operation from the instruction
+            ENW = 0;                //Don't let the register file write to the bus
+            IMM[5:0] = INST[5:0];        //Get the immediate value from the instruction
+            IMM[10:6] = 4'b0000;       //Set the other bit to 0
+            ALUcont = 4'b0011;            //Get the ALU operation from the instruction
         
         end else begin
             //Blah blahhhh...do nothing
         end
 end else if ( T == 2'b11 ) begin
     //2'b11:
-//         Gin = 0;                //Stop the G register from saving values
-//         ENW = 0;                //Don't let the register file write to the bus
+        Gin = 0;                //Stop the G register from saving values
+        ENW = 0;                //Don't let the register file write to the bus
 
-//         Gout = 1;               //Let the G register save the value from the bus
+        Gout = 1;               //Let the G register save the value from the bus
 
-//         Rin = Rx;               //Prep Rx register to save the value from the bus
-//         ENR = 1;                  //Let the register file to read the value from the bus
+        //Rin = Rx;               //Prep Rx register to save the value from the bus
+        ENR = 1;                  //Let the register file to read the value from the bus
 
-//         Clr = 1;                //Done with the operation, reset the counter
+        Clr = 1;                //Done with the operation, reset the counter
 // //endcase;
 end else begin
     //Do nothing
