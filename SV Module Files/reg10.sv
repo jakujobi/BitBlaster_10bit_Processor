24058483d99711110a8dd566e74b9e6b19fdb6eb
jakujobi <akujobi365@gmail.com>
2023-11-28 09:18:39 -0600

    Cleaned up documentation in the instruction register

--- SV Module Files/reg10.sv
+++ SV Module Files/reg10.sv
@@ -1,4 +1,10 @@
-//Instruction Register
+// Authors: John Akujobi,
+// Date: November, Fall, 2023
+// Name: instruction register
+// Filename: reg10.sv
+// Description: This module is a register
+//  that stores 10-bit values instruction from the shared data bus
+//  and is negative-edge clock triggered with synchronous active-high enable.
 
 module reg10 (
     input logic [9:0] D,    // 10-bit input data
@@ -8,14 +14,14 @@ module reg10 (
     output logic [9:0] Q    // 10-bit output data (register value)
 );
 
-    // The register logic
-    // Captures input D on the negative edge of CLKb when EN is high
-    always_ff @(negedge CLKb) begin
-        if (EN) begin
-            Q  <= D; // Load the input D into the register Q
-        end
-        // Note: If EN is low, the register retains its previous value
+// The register logic
+// Captures input D on the negative edge of CLKb when EN is high
+always_ff @(negedge CLKb) begin
+    if (EN) begin
+        Q  <= D;            // Load the input D into the register Q
     end
+    // Note: If EN is low, the register retains its previous value
+end
 
 endmodule
 
@@ -24,3 +30,10 @@ endmodule
 - This register is used to save the instruction at timestep 0, and maintain the instruction throughout the multiple clock cycles required to complete a given instruction.
 */
  
+// Authors: John Akujobi,
+// Date: November, Fall, 2023
+// Name: instruction register
+// Filename: reg10.sv
+// Description: This module is a register
+//  that stores 10-bit values instruction from the shared data bus
+//  and is negative-edge clock triggered with synchronous active-high enable.
