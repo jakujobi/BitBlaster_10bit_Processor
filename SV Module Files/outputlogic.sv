86815a96b06fe942929fdc0c1b9787465edf8b56
jakujobi <akujobi365@gmail.com>
2023-11-28 09:18:51 -0600

    Cleaned up documentation in the output logic module

--- SV Module Files/outputlogic.sv
+++ SV Module Files/outputlogic.sv
@@ -3,23 +3,26 @@
 // Name: Output Logic
 // Filename: outputlogic.sv
 // Description: This module is designed for visualizing internal processor signals. 
-// It provides a user-friendly interface to monitor the current state of the processor's data bus, register outputs, and current timestep.
-// The module drives LEDs and 7-segment displays based on the processor's internal data for real-time observation and debugging.
+//  It provides a user-friendly interface to monitor the current state of the processor's
+//   - internal data bus,
+//   - register outputs, and
+//   - current timestep.
+//  The module drives LEDs and 7-segment displays based on the processor's internal data for real-time observation and debugging.
 
 module outputlogic(
-    input logic [9:0] BUS,
-    input logic [9:0] REG,
-    input logic [1:0] TIME,
-    input logic DONE,
-    input logic Pkb,
+    input logic [9:0] BUS,      //Number from the shared data bus
+    input logic [9:0] REG,      //Number from the register file
+    input logic [1:0] TIME,     //Current timestep
+    input logic DONE,           //Done signal
+    input logic Pkb,            //Pkb signal from the peek button
 
-    output logic [9:0] LED_B,
-    output logic [6:0] DHEX0,
-    output logic [6:0] DHEX1,
-    output logic [6:0] DHEX2,
-    output logic [6:0] THEX,
-    output logic LED_D
-);
+    output logic [9:0] LED_B,   //LED_B shows either BUS or REG based on Pkb throgh the leds
+    output logic [6:0] DHEX0,   //DHEX2:0 shows either BUS or REG based on Pkb
+    output logic [6:0] DHEX1,   //  
+    output logic [6:0] DHEX2,   //DHEX2:0 shows either BUS or REG based on Pkb
+    output logic [6:0] THEX,    //HEX showing the current timestep
+    output logic LED_D          //LED showing the DONE signal
+);  
 
 
     // THEX shows the current timestep decoded to a 7-segment display
@@ -41,15 +44,17 @@ module outputlogic(
 
 endmodule
 
+//! Here, we used functions instead of having another module for converting values to 7-segment displays
+//! John learned this from a pdf manual of systemVerilog
 // Function to convert 2-bit time to 7-segment display
 function [6:0] time_to_7seg(input [1:0] processor_time);
     begin
-        case (processor_time)
-            2'b00: time_to_7seg = 7'b1000000; // Display "0"
-            2'b01: time_to_7seg = 7'b1111001; // Display "1"
-            2'b10: time_to_7seg = 7'b0100100; // Display "2"
-            2'b11: time_to_7seg = 7'b0110000; // Display "3"
-            default: time_to_7seg = 7'b1111111; // Display nothing
+        case (processor_time)   
+            2'b00: time_to_7seg = 7'b1000000;       // Display "0"
+            2'b01: time_to_7seg = 7'b1111001;       // Display "1"
+            2'b10: time_to_7seg = 7'b0100100;       // Display "2"
+            2'b11: time_to_7seg = 7'b0110000;       // Display "3"
+            default: time_to_7seg = 7'b1111111;     // Display nothing
         endcase
     end
 endfunction
@@ -59,9 +64,9 @@ function [20:0] value_to_7seg(input [9:0] value);
     logic [3:0] digit[2:0]; // Each digit of the 10-bit value
     begin
         // Splitting 10-bit value into 3 digits
-        digit[0] = value % 10;        // Least significant digit
-        digit[1] = (value / 10) % 10; // Middle digit
-        digit[2] = value / 100;       // Most significant digit
+        digit[0] = value % 10;                      // Least significant digit
+        digit[1] = (value / 10) % 10;               // Middle digit
+        digit[2] = value / 100;                     // Most significant digit
 
         // Convert each digit to 7-segment
         value_to_7seg = {digit_to_7seg(digit[2]), digit_to_7seg(digit[1]), digit_to_7seg(digit[0])};
@@ -73,24 +78,24 @@ function [6:0] digit_to_7seg(input [3:0] digit);
     begin
         case (digit)
             // Define cases for each digit (0-9)
-            4'b0000: digit_to_7seg  = 7'b1000000; // 0
-            4'b0001: digit_to_7seg  = 7'b1111001; // 1
-            4'b0010: digit_to_7seg  = 7'b0100100; // 2
-            4'b0011: digit_to_7seg  = 7'b0110000; // 3
-            4'b0100: digit_to_7seg  = 7'b0011001; // 4
-            4'b0101: digit_to_7seg  = 7'b0010010; // 5
-            4'b0110: digit_to_7seg  = 7'b0000010; // 6
-            4'b0111: digit_to_7seg  = 7'b1111000; // 7
-            4'b1000: digit_to_7seg  = 7'b0000000; // 8
-            4'b1001: digit_to_7seg  = 7'b0010000; // 9
-            4'b1010: digit_to_7seg  = 7'b0001000; // Display A
-            4'b1011: digit_to_7seg  = 7'b0000011; // Display B
-            4'b1100: digit_to_7seg  = 7'b1000110; // Display C
-            4'b1101: digit_to_7seg  = 7'b0100001; // Display D
-            4'b1110: digit_to_7seg  = 7'b0000110; // Display E
-            4'b1111: digit_to_7seg  = 7'b0001110; // Display F
+            4'b0000: digit_to_7seg  = 7'b1000000;   // 0
+            4'b0001: digit_to_7seg  = 7'b1111001;   // 1
+            4'b0010: digit_to_7seg  = 7'b0100100;   // 2
+            4'b0011: digit_to_7seg  = 7'b0110000;   // 3
+            4'b0100: digit_to_7seg  = 7'b0011001;   // 4
+            4'b0101: digit_to_7seg  = 7'b0010010;   // 5
+            4'b0110: digit_to_7seg  = 7'b0000010;   // 6
+            4'b0111: digit_to_7seg  = 7'b1111000;   // 7
+            4'b1000: digit_to_7seg  = 7'b0000000;   // 8
+            4'b1001: digit_to_7seg  = 7'b0010000;   // 9
+            4'b1010: digit_to_7seg  = 7'b0001000;   // Display A
+            4'b1011: digit_to_7seg  = 7'b0000011;   // Display B
+            4'b1100: digit_to_7seg  = 7'b1000110;   // Display C
+            4'b1101: digit_to_7seg  = 7'b0100001;   // Display D
+            4'b1110: digit_to_7seg  = 7'b0000110;   // Display E
+            4'b1111: digit_to_7seg  = 7'b0001110;   // Display F
 
-            default: digit_to_7seg = 7'b1111111; // Display nothing for invalid digit
+            default: digit_to_7seg = 7'b1111111;    // Display nothing for invalid digit
         endcase
     end
 endfunction
@@ -112,3 +117,15 @@ the current timestep decoded to a 7-segment display.
 - One final input/output of the output logic block takes the “Clr” signal from the controller.
 - If “Clr” is a logic-1, an LED DONE output should be active to indicate the current instruction has completed.
 */
+
+
+// Authors: John Akujobi,
+// Date: November, Fall, 2023
+// Name: Output Logic
+// Filename: outputlogic.sv
+// Description: This module is designed for visualizing internal processor signals. 
+//  It provides a user-friendly interface to monitor the current state of the processor's
+//   - internal data bus,
+//   - register outputs, and
+//   - current timestep.
+//  The module drives LEDs and 7-segment displays based on the processor's internal data for real-time observation and debugging.
