897240cb72aa7431233ef280dd2b3d36174868ad
jakujobi <akujobi365@gmail.com>
2023-11-28 09:19:05 -0600

    Cleaned up documentation in the inputlogic module

--- SV Module Files/inputlogic.sv
+++ SV Module Files/inputlogic.sv
@@ -8,21 +8,19 @@
 // the processor with external sources like switches and buttons.
 
 module inputlogic (
-    input logic [9:0] RawData,
-    input logic Peek_key,
-    input logic CLK_50MHz,
-    input logic RawCLK,
-    input Extrn_Enable,
+    input logic [9:0] RawData,      //10 bit raw data from external switches on the DE-10 Lite board
+    input logic Peek_key,           //Peek button 
+    input logic CLK_50MHz,          //50 MHz clock from DE-10 Lite
+    input logic RawCLK,             //Clock button from DE-10 Lite
+    input Extrn_Enable,             //This enables the external data to write into the shared data bus
 
-    output logic [9:0] databus,
-    output logic [1:0] data2bit,
-    output logic CLKb,
-    output logic PeeKb
+    output logic [9:0] databus,     //write to the shared data bus
+    output logic [1:0] data2bit,    //for the peek function and writes to RDA1, and it represents which register to be peeked into
+    output logic CLKb,              //debounced clock
+    output logic PeeKb              //debounced peek
 );
 
-//assign databus = 10'bz;
-
-//Debounce the keys__________________________________________
+//Debounce the keys________________
 debouncer peek_debouncer (
     .A_noisy(Peek_key),
     .CLK50M(CLK_50MHz),
@@ -35,13 +33,10 @@ debouncer clk_debouncer (
     .A(CLKb)
 );
 
-//splice the lowest 2 bits
-assign data2bit = RawData[1:0];
+assign data2bit = RawData[1:0];     //splice the lowest 2 bits into the data2bit output
 
-// //if extrn is enabled, assign raw data to databus
-// assign databus = Extrn_Enable ? RawData : z;
-
-//always comp block that assigns the data to the databus
+//always comp block that assigns the data to the databus when the external data is enabled
+//else the databus is set to 10'bz, which is a high impedance state
 always_comb begin
     if (Extrn_Enable) begin
         databus = RawData;
@@ -50,12 +45,5 @@ always_comb begin
         databus = 10'bz;
     end
 end
-
-// extrn external_data_receiver (
-//     .raw_data(raw_data),
-//     .extrn_enable (extrn_enable),
-//     .PKb(PKb)
-// );
-
 
 endmodule
